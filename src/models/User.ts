596d943fc39e7944f5484703518eef29376ee0c7
sebsolezzi88 <ssolezzi1@gmail.com>
2025-07-13 23:00:02 -0300

    Creacion del modelo user

---
+++ src/models/User.ts
@@ -0,0 +1,43 @@
+import mongoose,{Schema,Document} from "mongoose";
+
+export type Rol = "admin" | "user";
+
+export interface Location {
+    city:string;
+    province: string;
+}
+
+export interface IUser extends Document {
+  username: string;
+  name: string;
+  lastName: string;
+  bio?: string;
+  email: string;
+  password: string;
+  avatarUrl?: string;
+  location: Location;
+  role: Rol;
+  isActive: boolean;             // Se activa tras validar el email
+  activationToken?: string;      // Token para el enlace de activación
+  createdAt: Date;
+}
+
+const UserSchema = new Schema<IUser>({
+    username: {type: String, required:true, unique: true},
+    name: { type: String, required: true },
+    lastName: { type: String, required: true },
+    bio: { type: String },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    avatarUrl: { type: String },
+    location: {
+        city: { type: String, required: true },
+        country: { type: String, required: true },
+    },
+    role: { type: String, enum: ["admin", "user"], default: "user" },
+    isActive: { type: Boolean, default: false },
+    activationToken: { type: String },
+    createdAt: { type: Date, default: Date.now }
+})
+
+export const UserModel = mongoose.model<IUser>("User",UserSchema);
