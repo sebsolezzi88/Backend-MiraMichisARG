3b08333f2571aa225fd583157eb3628171a1d33a
sebsolezzi88 <ssolezzi1@gmail.com>
2025-07-14 21:51:24 -0300

    Añadiendo validacion de campos a la ruta de registro de usuarios

---
+++ src/middlewares/validateUserRegister.ts
@@ -0,0 +1,37 @@
+import {body} from 'express-validator';
+
+export const validateUserRegister = [
+  body('username')
+    .notEmpty().withMessage('Username is required')
+    .isLength({ min: 6 }).withMessage('Username must have at least 6 characters'),
+
+  body('name')
+    .notEmpty().withMessage('Name is required'),
+
+  body('lastName')
+    .notEmpty().withMessage('Last name is required'),
+
+  body('email')
+    .notEmpty().withMessage('Email is required')
+    .isEmail().withMessage('Email must be valid'),
+
+  body('password')
+    .notEmpty().withMessage('Password is required')
+    .isLength({ min: 6 }).withMessage('Password must have at least 6 characters'),
+
+  body('passwordrep')
+    .notEmpty().withMessage('Password confirmation is required'),
+
+  body('passwordrep').custom((value, { req }) => {
+    if (value !== req.body.password) {
+      throw new Error('Passwords must match');
+    }
+    return true;
+  }),
+
+  body('location.city')
+    .notEmpty().withMessage('City is required'),
+
+  body('location.country')
+    .notEmpty().withMessage('Country is required'),
+];
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { loginUser, registerUser } from "../controllers/userController";
+import { validateUserRegister } from "../middlewares/validateUserRegister";
 
 const router = Router();
 
-router.post('/register', registerUser);
+router.post('/register',validateUserRegister ,registerUser);
 router.post('/login', loginUser);
 
 export default router;
