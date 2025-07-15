0e6a36a1be4e24f979583d3a846580a5e0dabcfb
sebsolezzi88 <ssolezzi1@gmail.com>
2025-07-15 20:28:50 -0300

    Añadida la ruta para obtener un post por su id

--- src/routes/catPostRoutes.ts
+++ src/routes/catPostRoutes.ts
@@ -7,17 +7,12 @@ import { upload } from "../config/cloudinary";
 const router = Router();
 
 router.get('/', getAllCatPosts); //Obtener todos los post
-router.post('/',verifyToken,upload.single('photo'),createCatPost);
-router.put('/:id',verifyToken,upload.single('photo'),updateCatPostById);
+router.get('/:id', getCatPostById);   // Obtener un catpost por ID
+router.post('/',verifyToken,upload.single('photo'),createCatPost); //Crear post
+router.put('/:id',verifyToken,upload.single('photo'),updateCatPostById); //Actulizar post
 router.delete('/:id',verifyToken ,deleteCatPostById);      // Eliminar un catpost por ID
-router.get('/user/:userId', getCatPostsByUser); 
+router.get('/user/:userId', getCatPostsByUser);  //Obtener todos los post de un usuario
 router.patch('/:id/status',verifyToken ,updateCatPostStatus);//Actualizar estado activo/resuelto
-/* 
 
-router.get('/:id', getCatPostById);            // Obtener un catpost por ID
-
-router.get('/', getAllCatPosts);           //Obtener todos los post
-
-*/
 
 export default router;
