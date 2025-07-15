a78e765ac5a530c9d11efe8454505195ceb59010
sebsolezzi88 <ssolezzi1@gmail.com>
2025-07-15 10:25:55 -0300

    instalacion d emulter-storage-cloudinary

--- src/config/cloudinary.ts
+++ src/config/cloudinary.ts
@@ -1,5 +1,6 @@
 // config/cloudinary.ts
 import { v2 as cloudinary } from 'cloudinary';
+import { CloudinaryStorage } from 'multer-storage-cloudinary';
 import dotenv from 'dotenv';
 
 dotenv.config(); 
@@ -9,6 +10,16 @@ cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME!,
   api_key: process.env.CLOUDINARY_API_KEY!,
   api_secret: process.env.CLOUDINARY_SECRET_KEY!,
+});
+
+// Configurar Multer con Cloudinary
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: 'catposts',
+    allowed_formats: ['jpg', 'jpeg', 'png'],
+    transformation: [{ width: 800, height: 800, crop: 'limit' }],
+  } as any,
 });
 
 export default cloudinary;
