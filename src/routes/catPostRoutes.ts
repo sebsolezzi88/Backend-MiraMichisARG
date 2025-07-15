5cd69f3240e3206b5265577ddd64437dc2a8e94c
sebsolezzi88 <ssolezzi1@gmail.com>
2025-07-14 23:22:31 -0300

    Documentando rutas a crear

--- src/routes/catPostRoutes.ts
+++ src/routes/catPostRoutes.ts
@@ -3,9 +3,14 @@ import { Router } from "express";
 
 const router = Router();
 
-router.post('/',);
-router.get('/',);
-router.put('/',);
-router.delete('/');
+/* 
+router.post('/', createCatPost);               // Crear un nuevo catpost
+router.get('/:id', getCatPostById);            // Obtener un catpost por ID
+router.put('/:id', updateCatPostById);         // Actualizar un catpost por ID
+router.delete('/:id', deleteCatPostById);      // Eliminar un catpost por ID
+router.get('/', getAllCatPosts);           //Obtener todos los post
+router.get('/user/:userId', getCatPostsByUser); //Que un usuario obtenga sus post
+router.patch('/:id/status', updateCatPostStatus); //Actualizar estado activo/resuelto
+*/
 
 export default router;
