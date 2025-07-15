654256b1c970e80ae311948a78df73f86a2b853b
sebsolezzi88 <ssolezzi1@gmail.com>
2025-07-14 22:43:32 -0300

    Añadiendo controlador a la ruta

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { loginUser, registerUser } from "../controllers/userController";
+import { activateAccount, loginUser, registerUser } from "../controllers/userController";
 import { validateUserRegister } from "../middlewares/validateUserRegister";
 
 const router = Router();
 
 router.post('/register',validateUserRegister ,registerUser);
+router.get('/activate',activateAccount);
 router.post('/login', loginUser);
 
 export default router;
