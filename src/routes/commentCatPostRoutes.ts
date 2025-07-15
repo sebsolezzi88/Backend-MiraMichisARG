4a1cb2bd95a3d41426fea4d796871954011d21d1
sebsolezzi88 <ssolezzi1@gmail.com>
2025-07-15 20:48:33 -0300

    Creacion de rutas para los comentarios de Catpost

---
+++ src/routes/commentCatPostRoutes.ts
@@ -0,0 +1,15 @@
+import { Router } from "express";
+
+
+const router = Router();
+
+/* 
+    Rutas a crear
+    router.post('/:idCatPost',verifyToken,addComment); //El usuario creara comentarios
+    router.delete('/:idCatPost/comment/:idComment',verifyToken,deleteComment); //El usuario podrá borrar su comentario
+    router.get(/:idCatPost,getPostComment); // obtener los comentarios de un post
+    router.put('/:idCatPost/comment/:idComment',verifyToken,updateComment);  //Actualiza comentario
+*/
+
+
+export default router;
