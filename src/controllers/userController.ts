4c43ee202b415894f5af801194663beeb9421f88
sebsolezzi88 <ssolezzi1@gmail.com>
2025-07-14 22:24:17 -0300

    Añadiendo funcionalidad para mandar email luego de crear la cuenta

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { validationResult } from "express-validator";
 import User from "../models/User";
+import { transporter } from "../config/mail";
 
 
 export const registerUser = async (req:Request,res:Response):Promise<Response> =>{
@@ -11,8 +12,17 @@ export const registerUser = async (req:Request,res:Response):Promise<Response> =
             return res.status(400).json({errors:errors.array()}); //Si hay errores los enviamos
         }
         //Registrarmos al usuario.
-        await User.create(req.body);
-        return res.status(200).json({status:'success', message: "User created."});
+        const newUser =await User.create(req.body);
+
+        //Mandar mail
+        await transporter.sendMail({
+        from: '"Red Social de Michis 🐱" <no-reply@michinet.com>',
+        to: newUser.email,
+        subject: "Activa tu cuenta",
+        html: `<p>Hola ${newUser.name}, activa tu cuenta dando clic en el siguiente enlace:</p>
+                <a href="http://localhost:3000/activar/">Activar cuenta</a>`
+        });
+        return res.status(200).json({status:'success', message: "User created. We send a email confirmation"});
     } catch (error) {
         return res.status(500).json({status:'error', message: "Server Error."});
     }
