9c511d08524dd34be9a4551ca297130597a8e57f
sebsolezzi88 <ssolezzi1@gmail.com>
2025-07-14 21:55:02 -0300

    Agregando comprobacion de errores a registerUser

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,7 +1,14 @@
 import { Request, Response } from "express";
+import { validationResult } from "express-validator";
+
 
 export const registerUser = async (req:Request,res:Response):Promise<Response> =>{
     try {
+        //Comprobar errores
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            return res.status(400).json({errors:errors.array()}); //Si hay errores los enviamos
+        }
         return res.status(200).json({status:'success', message: "Listo."});
     } catch (error) {
         return res.status(500).json({status:'error', message: "Server Error."});
