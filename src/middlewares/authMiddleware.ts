906a09c2e893d81659d42e3fc5a3f2840e4bf866
sebsolezzi88 <ssolezzi1@gmail.com>
2025-07-15 10:12:53 -0300

    Creacion Middleware para verficar el token

---
+++ src/middlewares/authMiddleware.ts
@@ -0,0 +1,48 @@
+import {Request,Response,NextFunction} from 'express';
+import dotenv from 'dotenv';
+import  jwt  from 'jsonwebtoken';
+import User from '../models/User';
+
+
+//obtemos la palabra secreta de las variable de entorno
+dotenv.config();
+const JWT_SECRET = process.env.JWT_SECRET;
+
+interface DecodedToken extends jwt.JwtPayload {
+    id: string;
+    username: string;
+}
+
+export const verifyToken = async (req:Request,res:Response,next:NextFunction) => {
+    
+     /* Obtener el token de los headers de la solicitud 
+        Comúnmente viene en el header 'Authorization' con el prefijo 'Bearer '
+    */
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if(!token){
+         return res.status(401).json({ status:'error' ,message: 'token not provided' });
+    }
+
+    try {
+        const decoded = jwt.verify(token,JWT_SECRET!) as DecodedToken;
+        
+        //verificar si el usuario esta en la base de datos
+        const userExists = await User.findById(decoded.id);
+        if(!userExists){
+            return res.status(403).json({ status: 'error', message: 'invalid user' });
+        }
+       
+        next() //Continua la consulta 
+        
+    } catch (error:unknown) {
+        if (error instanceof jwt.TokenExpiredError) {
+            return res.status(401).json({ status: 'error', message: 'token expired' });
+        }
+        if (error instanceof jwt.JsonWebTokenError) {
+            return res.status(401).json({ status: 'error', message: 'invalid token' });
+        }
+        return res.status(500).json({status:'error', message:'internal server error',error});
+    }
+}
