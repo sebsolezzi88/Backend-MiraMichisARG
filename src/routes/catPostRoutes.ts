36f96d4c373c9aac8ef442b0ea0dd8f0931ebf87
sebsolezzi88 <ssolezzi1@gmail.com>
2025-07-15 19:51:39 -0300

    Agregando verificacion de token

--- src/routes/catPostRoutes.ts
+++ src/routes/catPostRoutes.ts
@@ -8,7 +8,7 @@ const router = Router();
 
 router.post('/',verifyToken,upload.single('photo'),createCatPost);
 router.put('/:id',verifyToken,upload.single('photo'),updateCatPostById);
-router.delete('/:id', deleteCatPostById);      // Eliminar un catpost por ID
+router.delete('/:id',verifyToken ,deleteCatPostById);      // Eliminar un catpost por ID
 /* 
 router.post('/', createCatPost);               // Crear un nuevo catpost
 router.get('/:id', getCatPostById);            // Obtener un catpost por ID
