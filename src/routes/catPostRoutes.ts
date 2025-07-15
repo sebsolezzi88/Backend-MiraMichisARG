f5fc0b1f5a72356008245342a55493365aa43035
sebsolezzi88 <ssolezzi1@gmail.com>
2025-07-15 20:11:41 -0300

    Añadiendo la ruta updateCatPostStatus

--- src/routes/catPostRoutes.ts
+++ src/routes/catPostRoutes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createCatPost, deleteCatPostById, getCatPostsByUser, updateCatPostById } from "../controllers/catPostController";
+import { createCatPost, deleteCatPostById, getCatPostsByUser, updateCatPostById, updateCatPostStatus } from "../controllers/catPostController";
 import { verifyToken } from "../middlewares/authMiddleware";
 import { upload } from "../config/cloudinary";
 
@@ -10,13 +10,14 @@ router.post('/',verifyToken,upload.single('photo'),createCatPost);
 router.put('/:id',verifyToken,upload.single('photo'),updateCatPostById);
 router.delete('/:id',verifyToken ,deleteCatPostById);      // Eliminar un catpost por ID
 router.get('/user/:userId', getCatPostsByUser); 
+router.patch('/:id/status',verifyToken ,updateCatPostStatus);//Actualizar estado activo/resuelto
 /* 
 
 router.get('/:id', getCatPostById);            // Obtener un catpost por ID
 
 router.get('/', getAllCatPosts);           //Obtener todos los post
 
-router.patch('/:id/status', updateCatPostStatus); //Actualizar estado activo/resuelto
+router.patch('/:id/status', updateCatPostStatus); 
 */
 
 export default router;
