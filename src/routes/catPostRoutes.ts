4cd160b496b2eacd917e15cce51e48572c20d0b1
sebsolezzi88 <ssolezzi1@gmail.com>
2025-07-15 20:22:22 -0300

    Agrega la ruta para obtener todos los post

--- src/routes/catPostRoutes.ts
+++ src/routes/catPostRoutes.ts
@@ -6,6 +6,7 @@ import { upload } from "../config/cloudinary";
 
 const router = Router();
 
+router.get('/', getAllCatPosts); //Obtener todos los post
 router.post('/',verifyToken,upload.single('photo'),createCatPost);
 router.put('/:id',verifyToken,upload.single('photo'),updateCatPostById);
 router.delete('/:id',verifyToken ,deleteCatPostById);      // Eliminar un catpost por ID
@@ -17,7 +18,6 @@ router.get('/:id', getCatPostById);            // Obtener un catpost por ID
 
 router.get('/', getAllCatPosts);           //Obtener todos los post
 
-router.patch('/:id/status', updateCatPostStatus); 
 */
 
 export default router;
