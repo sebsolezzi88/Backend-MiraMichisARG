d62c90bead2390aab13d2480cab05230836c6f42
sebsolezzi88 <ssolezzi1@gmail.com>
2025-07-15 22:41:19 -0300

    Funcionalidad para obtener los comentarios de un post

--- src/controllers/catCommentController.ts
+++ src/controllers/catCommentController.ts
@@ -1,5 +1,7 @@
 import { Response,Request } from "express";
 import { ObjectId } from "mongoose";
+import CatPost from "../models/CatPost";
+import Comment from "../models/Commet";
 
 interface CustomRequest extends Request {
     userId?: ObjectId;
@@ -8,10 +10,17 @@ interface CustomRequest extends Request {
 
 export const getPostComments = async (req: CustomRequest, res: Response): Promise<Response> => {
   try {
+    const {catPostId} = req.params; //id del post a comentario
     
+    const existingPost = await CatPost.findById(catPostId);
     
-    
-    return res.status(200).json({ status:"success", message: "ok"});
+    if(!existingPost){
+      return res.status(404).json({ status:"error", message: "Post not Found"});
+    }
+    //Encontrar los comentarios del post
+    const existingComments = await Comment.find({catPostId}).populate('userId','username');
+        
+    return res.status(200).json({ status:"success", message: "ok", post: existingPost,comments: existingComments});
 
   } catch (error) {
     console.error("Error delete post:", error);
@@ -21,12 +30,26 @@ export const getPostComments = async (req: CustomRequest, res: Response): Promis
 export const addComment = async (req: CustomRequest, res: Response): Promise<Response> => {
   try {
     
+
+    const {catPostId} = req.params; //id del post a comentario
+    const userId = req.userId; //id del usuario que comenta
+    const {text} = req.body; //Comentario del usuario
+
+    if(!text || text === ''){
+      return res.status(400).json({ status:"error", message: "The text is required"});
+    }
+
+    const existingPost = CatPost.findById(catPostId);
+    if(!existingPost){
+      return res.status(404).json({ status:"error", message: "Post not Found"});
+    }
+
+    const newComment = await Comment.create({catPostId,userId,text});
     
-    
-    return res.status(200).json({ status:"success", message: "ok"});
+    return res.status(200).json({ status:"success", message: "Comment created", comment: newComment});
 
   } catch (error) {
-    console.error("Error delete post:", error);
+    console.error("Error:", error);
     return res.status(500).json({ status:"error", message: "Server error" });
   }
 }
