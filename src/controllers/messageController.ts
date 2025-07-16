51aeb350ef13af94205da04e59d06d9ee429fed3
sebsolezzi88 <ssolezzi1@gmail.com>
2025-07-16 10:31:10 -0300

    Creada la sendMessage

---
+++ src/controllers/messageController.ts
@@ -0,0 +1,44 @@
+import { Request, Response } from "express";
+import { isValidObjectId, ObjectId } from "mongoose";
+import User from "../models/User";
+import Message from "../models/Message";
+
+interface CustomRequest extends Request {
+    userId?: ObjectId;
+    
+}
+
+export const sendMessage = async (req: CustomRequest, res: Response): Promise<Response> => {
+    try {
+        const {text,toUserId} = req.body;
+
+        if(!text){
+            return res.status(400).json({ status:"error", message: "Text required"});
+        }
+        if(!toUserId || !isValidObjectId(toUserId)){
+            return res.status(400).json({ status:"error", message: "Invalid ID"});
+        }
+
+        //Comprobar si el receptor existe
+        const existUser = User.findById(toUserId);
+        if(!existUser){
+            return res.status(404).json({ status:"error", message: "User not found"});
+        }
+
+        //Si existe mandamos el mensaje
+        const newMessage = await Message.create({
+            fromUserId: req.userId,
+            toUserId: toUserId,
+            text: text
+        })
+
+        return res.status(200).json({ 
+            status:"success", 
+            message: "Message send",
+            newMessage
+        });
+    } catch (error) {
+        console.error("Error send Message:", error);
+        return res.status(500).json({ status:"error", message: "Server error" });
+    }
+}
