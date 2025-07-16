c80a331d0d199532a662daa2cbd5acea81408eff
sebsolezzi88 <ssolezzi1@gmail.com>
2025-07-15 23:02:41 -0300

    Cambio en ruta  de put

--- src/routes/catCommentRoutes.ts
+++ src/routes/catCommentRoutes.ts
@@ -9,7 +9,7 @@ const router = Router();
 router.get('/:catPostId/comments', getPostComments); // Obtener todos los comentarios de un post
 router.post('/:catPostId/comments', verifyToken, addComment); // Crear comentario
 router.delete('/comments/:commentId', verifyToken, deleteComment); // Eliminar un comentario
-router.put('/:catPostId/comments/:commentId', verifyToken, updateComment); // Editar un comentario
+router.put('/comments/:commentId', verifyToken, updateComment); // Editar un comentario
 
 
 
