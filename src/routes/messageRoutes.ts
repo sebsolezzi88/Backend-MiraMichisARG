fb6f4f99e150d45c28f56487776c8c0961cfec36
sebsolezzi88 <ssolezzi1@gmail.com>
2025-07-16 10:59:41 -0300

    Agregadas rutas para ver mensajes recibidos y enviados

--- src/routes/messageRoutes.ts
+++ src/routes/messageRoutes.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import { verifyToken } from "../middlewares/authMiddleware";
-import { sendMessage } from "../controllers/messageController";
+import { getMessageReceived, getMessageSended, sendMessage } from "../controllers/messageController";
 
 
 
 const router = Router();
 
 router.post('/',verifyToken,sendMessage); //Ruta para enviar mensaje
+router.get('/send',verifyToken,getMessageSended); //Ruta para obtener los mensajes recibidos
+router.get('/inbox',verifyToken,getMessageReceived); //Ruta para ver los mensajes recibidos
 
 
 export default router;
