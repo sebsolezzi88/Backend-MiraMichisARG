35891ded5e65f7b547e71958a1ca6c9412405377
sebsolezzi88 <ssolezzi1@gmail.com>
2025-07-16 21:46:07 -0300

    Crear funcion para actualizar perfil de usuario

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -120,3 +120,26 @@ export const loginUser = async (req:Request,res:Response):Promise<Response> =>{
         return res.status(500).json({ message: 'Server error' });
     }
 }
+
+export const updateProfile = async (req:Request,res:Response):Promise<Response> =>{
+    const errors = validationResult(req);
+    
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { username, password } = req.body;
+
+    try {
+        const user = await User.findOne({ username });
+
+        //El usuario no esta registrado
+        if (!user) {
+            return res.status(401).json({ message: 'Invalid credentials' });
+        }
+
+        return res.status(200).json({ message: 'Update successful' });
+    } catch (error) {
+        return res.status(500).json({ message: 'Server error' });
+    }
+}
